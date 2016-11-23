33b90e6df2a550b0cbaa44d6544c75964e99ed49
yugalgautam <yugalgautam97@yahoo.co.in>
2016-11-23 22:06:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/screencapture-file-C-Users-YUGAL-Desktop-SoCon-16UI-htm-1472985729727.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'imad-2016-app/ui/screencapture-file-C-Users-YUGAL-Desktop-SoCon-16UI-htm-1472985729727.png'));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
